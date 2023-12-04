0d53820f6043b0c26d591a4b19edfc1a9aed78e5
Laurent Jouglar <ljouglar@ipneos.com>
2023-12-04 09:46:31 +0100

    day 4 part 1 solved

--- aoc_04/aoc_0401.ts
+++ aoc_04/aoc_0401.ts
@@ -2,18 +2,34 @@ import { readFileSync } from 'fs';
 
 type Lists = {
   winningCards: Array<string>;
-  cards: Array<string>
-}
+  cards: Array<string>;
+};
 
-const filename = 'aoc_04/aoc_0401.data';
+const filename = 'aoc_04/aoc_04.data';
 
 const extractLines = (): Array<string> => readFileSync(filename, 'utf-8').split('\n');
 
 const mapLineToLists = (line: string): Lists => ({
-  winningCards: line.split(': ')[1].split(' | ')[0].split(/ +/),
-  cards: line.split(': ')[1].split(' | ')[1].split(/ +/),
-})
+  winningCards: line
+    .split(/: +/)[1]
+    .split(/ +\| +/)[0]
+    .split(/ +/),
+  cards: line
+    .split(/: +/)[1]
+    .split(/ +\| +/)[1]
+    .split(/ +/),
+});
 
-const result = extractLines().map(mapLineToLists).reduce((length, curList) => length + 1, 0);
+const mapListsToInteresect = (lists: Lists): Array<string> =>
+  lists.winningCards.filter((card: string) => lists.cards.includes(card));
+
+const mapIntersectToValue = (intersect: Array<string>): number =>
+  intersect.length === 0 ? 0 : 2 ** (intersect.length - 1);
+
+const result = extractLines()
+  .map(mapLineToLists)
+  .map(mapListsToInteresect)
+  .map(mapIntersectToValue)
+  .reduce((sum, curValue) => sum + curValue, 0);
 
 console.log(`result ${result}`);

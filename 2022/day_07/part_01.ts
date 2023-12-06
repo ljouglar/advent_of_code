3164afdb8689a05ff1a5a8f20a1607d1fb6cb651
Laurent Jouglar <ljouglar@ipneos.com>
2023-12-06 12:05:46 +0100

    tree building and printing

--- 2022/day_07/part_01.ts
+++ 2022/day_07/part_01.ts
@@ -1,18 +1,65 @@
 import { readFileSync } from 'fs';
 
 type TreeNode = {
-  [key: string]: any;
+  nodeType: 'dir' | 'file';
+  name: string;
   children?: TreeNode[];
   size: number;
-}
-const filename = '2022/day_07/input.data';
+  parent: TreeNode | null;
+};
+const filename = '2022/day_07/input.ex.data';
 
 const extractLines = (): Array<string> => readFileSync(filename, 'utf-8').split('\n');
 
-// const tree: Array<Leave> = [];
-// for (const line: string of extractLines()) {
-  
-// }
+const rootNode: TreeNode = { nodeType: 'dir', name: '/', children: [], size: 0, parent: null };
+let currentNode: TreeNode = undefined;
+let destNode: TreeNode = undefined;
+for (const line of extractLines()) {
+  if (line.match(/^\$ cd/)) {
+    const dest = line.split(' ').pop();
+    switch (dest) {
+      case '/':
+        currentNode = rootNode;
+        break;
+      case '..':
+        currentNode = currentNode.parent;
+        break;
+      default:
+        destNode = currentNode.children.find((node: TreeNode) => node.nodeType === 'dir' && node.name === dest);
+        if (!destNode) {
+          destNode = { nodeType: 'dir', name: dest, children: [], size: 0, parent: currentNode };
+          currentNode.children.push(destNode);
+        }
+        currentNode = destNode;
+        break;
+    }
+  } else if (line.match(/^dir /)) {
+    const dest = line.split(' ').pop();
+    destNode = currentNode.children.find((node: TreeNode) => node.nodeType === 'dir' && node.name === dest);
+    if (!destNode) {
+      destNode = { nodeType: 'dir', name: dest, children: [], size: 0, parent: currentNode };
+      currentNode.children.push(destNode);
+    }
+  } else if (line.match(/^\d+ /)) {
+    const [size, name] = line.split(' ');
+    destNode = currentNode.children.find((node: TreeNode) => node.nodeType === 'file' && node.name === name);
+    if (!destNode) {
+      destNode = { nodeType: 'file', name, children: [], size: +size, parent: currentNode };
+      currentNode.children.push(destNode);
+    }
+  }
+}
+
+const printTree = (node: TreeNode, deep: number = 0): void => {
+  console.log(
+    '  '.repeat(deep) + ' - ' + node.name + ' (' + node.nodeType + (node.nodeType === 'dir' ? ')' : `, ${node.size})`),
+  );
+  for (const child of node.children) {
+    printTree(child, deep + 1);
+  }
+};
+
+printTree(rootNode);
 
 const result = extractLines().length;
 

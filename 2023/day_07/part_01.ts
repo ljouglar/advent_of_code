08bf35389e6e81f6eca0758b024c81bd720aac98
Laurent Jouglar <ljouglar@ipneos.com>
2023-12-07 13:34:48 +0100

    day 7 part 1 solved

--- 2023/day_07/part_01.ts
+++ 2023/day_07/part_01.ts
@@ -15,27 +15,30 @@ type Hand = {
   cards: string;
   bid: number;
   type: HandTypes;
+  rank: number;
 };
 
 const CardValues = 'AKQJT98765432';
 
-const filename = '2023/day_07/input.ex.data';
+const filename = '2023/day_07/input.data';
 
 const extractLines = (): Array<string> => readFileSync(filename, 'utf-8').split('\n');
 
 const getHandType = (cards: string): HandTypes => {
-  // TOUT FAUX !!
-  if (cards.match(/.{5}/)) {
+  const qties = cards
+    .split('')
+    .reduce((qties, curChar) => ({ ...qties, [curChar]: curChar in qties ? qties[curChar] + 1 : 1 }), {});
+  if (Object.values(qties).includes(5)) {
     return HandTypes.FiveOfAKind;
-  } else if (cards.match(/.{4}/)) {
+  } else if (Object.values(qties).includes(4)) {
     return HandTypes.FourOfAKind;
-  } else if (cards.match(/.{3}.{2}/) || cards.match(/.{1}.{3}/)) {
+  } else if (Object.values(qties).includes(3) && Object.values(qties).includes(2)) {
     return HandTypes.FullHouse;
-  } else if (cards.match(/.{3}/)) {
+  } else if (Object.values(qties).includes(3)) {
     return HandTypes.ThreeOfAKind;
-  } else if (cards.match(/.{2}.*.{2}/)) {
+  } else if (Object.values(qties).filter((x) => x === 2).length === 2) {
     return HandTypes.TwoPair;
-  } else if (cards.match(/.{2}/)) {
+  } else if (Object.values(qties).includes(2)) {
     return HandTypes.OnePair;
   }
   return HandTypes.HighCard;
@@ -48,8 +51,25 @@ const hands: Array<Hand> = extractLines()
       cards: values[0],
       bid: +values[1],
       type: getHandType(values[0]),
+      rank: undefined,
     }),
   );
 
+const compareHands = (hand1: Hand, hand2: Hand): number => {
+  for (const i of [0, 1, 2, 3, 4]) {
+    const diff = CardValues.indexOf(hand2.cards[i]) - CardValues.indexOf(hand1.cards[i]);
+    if (diff !== 0) {
+      return diff;
+    }
+  }
+  return 0;
+};
 
-console.log(`result ${JSON.stringify(hands)}`);
+let rank: number = 1;
+for (const handType of Object.values(HandTypes).reverse()) {
+  hands.filter((hand: Hand) => hand.type === handType).sort(compareHands).forEach((hand) => hand.rank = rank++);
+}
+
+const result = hands.reduce((sum: number, curHand: Hand): number => sum + curHand.bid * curHand.rank, 0)
+
+console.log(`result ${result}`);
